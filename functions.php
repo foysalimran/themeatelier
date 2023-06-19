45ee08595db86f004ebc0bb487237b346da907d5
foysalimran <fimran2011@gmail.com>
2023-06-19 12:11:08 +0600

    init codestar

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
-
+require('lib/codestar/codestar-framework.php');
+include_once('inc/admin-options.php');
+include_once('inc/metabox-options.php');
 // ADD THEME SUPPORT
 function themeatelier_initialize()
 {
