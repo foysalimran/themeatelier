f9e93a7049a7f5c1ba17b6dd92e321b86135826b
foysalimran <fimran2011@gmail.com>
2023-07-25 09:45:57 +0600

    update

--- page.php
+++ page.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-<section class="">
-      <div class="container px-4 m-auto text-center">
-        <h1 class="pt-40 mb-0 text-4xl "><?php the_title(); ?></h1>
+<?php while (have_posts()) : the_post(); ?>
+
+
+  <section class="h-[380px] bg-secondary">
+    <div class="container text-center">
+      <h1 class="mb-0 text-3xl sm:text-4xl pt-44">
+        <?php the_title(); ?>
+      </h1>
+
+    </div>
+  </section>
+
+  <section class="pt-16 pb-24 bg-white">
+    <div class="container px-4 m-auto text-lg font-normal sp-single-page">
+
+      <div class="entry-content prose lg:prose-xl">
+        <?php the_content(); ?>
       </div>
-    </section>
-    
-    <section class="pt-16 pb-24 bg-white">
-      <div class="container px-4 m-auto text-lg font-normal sp-single-page">
-        <article
-          id="page-475"
-          class="post-475 page type-page status-publish hentry"
-        >
-          <div class="entry-content">
-            <?php the_content(); ?>
-          </div>
-          <!-- .entry-content -->
-        </article>
-      </div>
-    </section>
+      <!-- .entry-content -->
+
+    </div>
+  </section>
 <?php endwhile; ?>
 <?php get_footer(); ?>
