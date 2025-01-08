c1d7e2f298e5a3c1392879dd66b56938e362b48d
Foysal Imran <fimran2011@gmail.com>
2025-01-08 12:03:03 +0600

    update

--- page-templates/product-showcase/pricing.php
+++ page-templates/product-showcase/pricing.php
@@ -2,9 +2,9 @@
     <div class="container px-4 mx-auto">
         <div class="max-w-3xl mx-auto mb-12 text-center">
             <h2 class="text-ta-section-title -mt-1.5 mb-0">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias, dolor.
+            Affordable Pricing For Everyone
             </h2>
-            <span class="inline-block mt-5 text-lg font-normal">Unlock Limitless Possibilities with our Empowering Suite of WordPress Plugins</span>
+            <span class="inline-block mt-5 text-lg font-normal">Pick a plan based on how many websites you need to use WhatsApp Chat Help.</span>
         </div>
         <!-- Tabs -->
         <div class="flex justify-center gap-3 mb-2 shadow-md w-[fit-content] rounded-md mx-auto bg-white p-3">
@@ -18,80 +18,80 @@
                         <h3 class="text-2xl text-center">Single Site</h3>
                         <p class="text-center">Best Choice for Individuals</p>
                         <div class="flex items-end justify-center">
-                            <s class="mt-1.5 mr-2.5">$49</s>
                             <p class="mb-0 text-lg font-bold">$</p>
-                            <h4 class="text-4xl font-bold">39</h4>
+                            <h4 class="text-4xl font-bold">29</h4>
                             <p class="mb-0">/ Yearly</p>
                         </div>
                         <hr class="mt-10 mb-10 bg-secondary text-secondary">
-                        <a target="_blank" href="https://wpchatplugins.com/whatsapp-chat-support" class="w-full text-center ta-btn-secondary group">Buy Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options[price_id]=1" class="w-full text-center ta-btn-secondary group">Buy Now</a>
                         <div class="h-8"></div>
                         <ul class="features-list">
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Activation on 1 Website</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Standard Support for 1 Year</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Automatic Updates for 1 Year</strong>
                             </li>
+                            <li>14-Day Moneyback Guarantee</li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            All Pro Features Included
                             </li>
                         </ul>
                     </div>
                     <div class="p-6 lg:p-10 pricing_item border-[1.5px] border-solid border-[#b0b3bc] lg:border-0 lg:border-none rounded-md mb-4 lg:rounded-none lg:mb-0 popular">
-                        <h3 class="text-2xl text-center">Single Site</h3>
-                        <p class="text-center">Best Choice for Individuals</p>
+                        <h3 class="text-2xl text-center">Five Sites</h3>
+                        <p class="text-center">Best Choice for Small Business and Freelancers</p>
                         <div class="flex items-end justify-center">
-                            <s class="mt-1.5 mr-2.5">$49</s>
                             <p class="mb-0 text-lg font-bold">$</p>
-                            <h4 class="text-4xl font-bold">39</h4>
+                            <h4 class="text-4xl font-bold">99</h4>
                             <p class="mb-0">/ Yearly</p>
                         </div>
                         <hr class="mt-10 mb-10 bg-secondary text-secondary">
-                        <a target="_blank" href="https://wpchatplugins.com/whatsapp-chat-support" class="w-full text-center ta-btn-secondary group">Buy Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options[price_id]=2" class="w-full text-center ta-btn-secondary group">Buy Now</a>
                         <div class="h-8"></div>
                         <ul class="features-list">
-                            <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                        <li>
+                            <strong>Activation on 5 Websites</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Priority Support for 1 Year</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Automatic Updates for 1 Year</strong>
                             </li>
+                            <li>14-Day Moneyback Guarantee</li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            All Pro Features Included
                             </li>
                         </ul>
                     </div>
                     <div class="p-6 lg:p-10 pricing_item border-[1.5px] border-solid border-[#b0b3bc] lg:border-0 lg:border-none rounded-md mb-4 lg:rounded-none lg:mb-0">
-                        <h3 class="text-2xl text-center">Single Site</h3>
-                        <p class="text-center">Best Choice for Individuals</p>
+                        <h3 class="text-2xl text-center">Unlimited Sites</h3>
+                        <p class="text-center">Best Choice for Agencies and Developers</p>
                         <div class="flex items-end justify-center">
-                            <s class="mt-1.5 mr-2.5">$49</s>
                             <p class="mb-0 text-lg font-bold">$</p>
-                            <h4 class="text-4xl font-bold">39</h4>
+                            <h4 class="text-4xl font-bold">199</h4>
                             <p class="mb-0">/ Yearly</p>
                         </div>
                         <hr class="mt-10 mb-10 bg-secondary text-secondary">
-                        <a target="_blank" href="https://wpchatplugins.com/whatsapp-chat-support" class="w-full text-center ta-btn-secondary group">Buy Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options[price_id]=3" class="w-full text-center ta-btn-secondary group">Buy Now</a>
                         <div class="h-8"></div>
                         <ul class="features-list">
-                            <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                        <li>
+                            <strong>Activation on Unlimited Website</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Priority Support for 1 Year</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Automatic Updates for 1 Year</strong>
                             </li>
+                            <li>14-Day Moneyback Guarantee</li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            All Pro Features Included
                             </li>
                         </ul>
                     </div>
@@ -104,80 +104,80 @@
                         <h3 class="text-2xl text-center">Single Site</h3>
                         <p class="text-center">Best Choice for Individuals</p>
                         <div class="flex items-end justify-center">
-                            <s class="mt-1.5 mr-2.5">$49</s>
                             <p class="mb-0 text-lg font-bold">$</p>
-                            <h4 class="text-4xl font-bold">42</h4>
+                            <h4 class="text-4xl font-bold">129</h4>
                             <p class="mb-0">/ Yearly</p>
                         </div>
                         <hr class="mt-10 mb-10 bg-secondary text-secondary">
-                        <a target="_blank" href="https://wpchatplugins.com/whatsapp-chat-support" class="w-full text-center ta-btn-secondary group">Buy Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options[price_id]=4" class="w-full text-center ta-btn-secondary group">Buy Now</a>
                         <div class="h-8"></div>
                         <ul class="features-list">
-                            <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                        <li>
+                            <strong>Activation on 1 Website</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Standard Support for Lifetime</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Automatic Updates for Lifetime</strong>
                             </li>
+                            <li>14-Day Moneyback Guarantee</li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            All Pro Features Included
                             </li>
                         </ul>
                     </div>
                     <div class="p-6 lg:p-10 pricing_item border-[1.5px] border-solid border-[#b0b3bc] lg:border-0 lg:border-none rounded-md mb-4 lg:rounded-none lg:mb-0 popular">
-                        <h3 class="text-2xl text-center">Single Site</h3>
-                        <p class="text-center">Best Choice for Individuals</p>
+                        <h3 class="text-2xl text-center">Five Sites</h3>
+                        <p class="text-center">Best Choice for Small Business and Freelancers</p>
                         <div class="flex items-end justify-center">
-                            <s class="mt-1.5 mr-2.5">$49</s>
                             <p class="mb-0 text-lg font-bold">$</p>
-                            <h4 class="text-4xl font-bold">42</h4>
+                            <h4 class="text-4xl font-bold">229</h4>
                             <p class="mb-0">/ Yearly</p>
                         </div>
                         <hr class="mt-10 mb-10 bg-secondary text-secondary">
-                        <a target="_blank" href="https://wpchatplugins.com/whatsapp-chat-support" class="w-full text-center ta-btn-secondary group">Buy Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options[price_id]=5" class="w-full text-center ta-btn-secondary group">Buy Now</a>
                         <div class="h-8"></div>
                         <ul class="features-list">
-                            <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                        <li>
+                            <strong>Activation on 5 Websites</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Priority Support for Lifetime</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Automatic Updates for Lifetime</strong>
                             </li>
+                            <li>14-Day Moneyback Guarantee</li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            All Pro Features Included
                             </li>
                         </ul>
                     </div>
                     <div class="p-6 lg:p-10 pricing_item border-[1.5px] border-solid border-[#b0b3bc] lg:border-0 lg:border-none rounded-md mb-4 lg:rounded-none lg:mb-0">
-                        <h3 class="text-2xl text-center">Single Site</h3>
-                        <p class="text-center">Best Choice for Individuals</p>
+                        <h3 class="text-2xl text-center">Unlimited Sites</h3>
+                        <p class="text-center">Best Choice for Agencies and Developers</p>
                         <div class="flex items-end justify-center">
-                            <s class="mt-1.5 mr-2.5">$49</s>
                             <p class="mb-0 text-lg font-bold">$</p>
-                            <h4 class="text-4xl font-bold">42</h4>
+                            <h4 class="text-4xl font-bold">399</h4>
                             <p class="mb-0">/ Yearly</p>
                         </div>
                         <hr class="mt-10 mb-10 bg-secondary text-secondary">
-                        <a target="_blank" href="https://wpchatplugins.com/whatsapp-chat-support" class="w-full text-center ta-btn-secondary group">Buy Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options[price_id]=6" class="w-full text-center ta-btn-secondary group">Buy Now</a>
                         <div class="h-8"></div>
                         <ul class="features-list">
-                            <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                        <li>
+                            <strong>Unlimited Websites</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Priority Support for Lifetime</strong>
                             </li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            <strong>Automatic Updates for Lifetime</strong>
                             </li>
+                            <li>14-Day Moneyback Guarantee</li>
                             <li>
-                                Lorem ipsum dolor sit consectetur elit.
+                            All Pro Features Included
                             </li>
                         </ul>
                     </div>
