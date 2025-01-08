952af2d3e11dd0af190ebed82cc033bbd19ae5fe
Foysal Imran <fimran2011@gmail.com>
2025-01-08 18:00:11 +0600

    update

--- page-templates/product-showcase/backend-screenshots.php
+++ page-templates/product-showcase/backend-screenshots.php
@@ -7,38 +7,83 @@
             <span class="inline-block mt-5 text-lg font-normal">Get a wide range of powerful and easy-to-use features and customizations, as seen in the following backend screenshot.</span>
         </div>
         <div class="gallery">
-            <div class="grid items-center grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6 xl:gap-8 md:grid-cols-3 lg:grid-cols-4">
-                <a class="gallery_item" href="https://images.pexels.com/photos/29907156/pexels-photo-29907156.jpeg" data-fancybox="gallery">
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6 xl:gap-8 md:grid-cols-3 lg:grid-cols-4">
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-1.png" data-fancybox="gallery">
                     <div class="gallery_item_icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                         </svg>
                     </div>
-                    <img src="https://images.pexels.com/photos/29907156/pexels-photo-29907156.jpeg" alt="Thumbnail 1" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-1.png" alt="Thumbnail 1" />
                 </a>
-                <a class="gallery_item" href="https://images.pexels.com/photos/9807701/pexels-photo-9807701.jpeg" data-fancybox="gallery">
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-2.png" data-fancybox="gallery">
                     <div class="gallery_item_icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                         </svg>
                     </div>
-                    <img src="https://images.pexels.com/photos/9807701/pexels-photo-9807701.jpeg" alt="Thumbnail 2" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-2.png" alt="Thumbnail 2" />
                 </a>
-                <a class="gallery_item" href="https://images.pexels.com/photos/29636713/pexels-photo-29636713.jpeg" data-fancybox="gallery">
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-3.png" data-fancybox="gallery">
                     <div class="gallery_item_icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                         </svg>
                     </div>
-                    <img src="https://images.pexels.com/photos/29636713/pexels-photo-29636713.jpeg" alt="Thumbnail 3" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-3.png" alt="Thumbnail 3" />
                 </a>
-                <a class="gallery_item" href="https://images.pexels.com/photos/8504955/pexels-photo-8504955.jpeg" data-fancybox="gallery">
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-4.png" data-fancybox="gallery">
                     <div class="gallery_item_icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                         </svg>
                     </div>
-                    <img src="https://images.pexels.com/photos/8504955/pexels-photo-8504955.jpeg" alt="Thumbnail 3" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-4.png" alt="Thumbnail 3" />
+                </a>
+
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-5.png" data-fancybox="gallery">
+                    <div class="gallery_item_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                        </svg>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-5.png" alt="Thumbnail 3" />
+                </a>
+
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-6.png" data-fancybox="gallery">
+                    <div class="gallery_item_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                        </svg>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-6.png" alt="Thumbnail 3" />
+                </a>
+
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-7.png" data-fancybox="gallery">
+                    <div class="gallery_item_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                        </svg>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-7.png" alt="Thumbnail 3" />
+                </a>
+
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-8.png" data-fancybox="gallery">
+                    <div class="gallery_item_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                        </svg>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-8.png" alt="Thumbnail 3" />
+                </a>
+
+                <a class="gallery_item" href="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-9.png" data-fancybox="gallery">
+                    <div class="gallery_item_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                        </svg>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/wa-chat-help-ss/screenshot-9.png" alt="Thumbnail 3" />
                 </a>
             </div>
         </div>
