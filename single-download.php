2f238b89bfa241e1ec01d19bc165914598f24df7
Foysal Imran <fixolab@JS-iMac.bbrouter>
2025-02-01 12:23:09 +0600

    update

--- single-download.php
+++ single-download.php
@@ -1,8 +1,11 @@
 <?php
 $selected_sections = get_post_meta(get_the_ID(), 'page_metabox', true);
 $section_group = !empty($selected_sections) ? $selected_sections['section_group'] : [];
-$select_sections = !empty($section_group) ? $section_group['select_section'] : [];
+$select_sections = isset($section_group['select_section']) ? $section_group['select_section'] : [];
 $color_settings = !empty($selected_sections) ? $selected_sections['color_settings'] : [];
+$color_primary = isset($color_settings['primary']) ? $color_settings['primary'] : '';
+$color_secondary = isset($color_settings['secondary']) ? $color_settings['secondary'] : '';
+$color_dark = isset($color_settings['dark']) ? $color_settings['dark'] : '';
 
 get_template_part('cwp-header');
 ?>
@@ -12,9 +15,9 @@ get_template_part('cwp-header');
     }
 
     :root {
-        --primary: <?php echo esc_attr($color_settings['primary']) ?>;
-        --secondary: <?php echo esc_attr($color_settings['secondary']) ?>;
-        --dark: <?php echo esc_attr($color_settings['dark']) ?>;
+        --primary: <?php echo esc_attr($color_primary) ?>;
+        --secondary: <?php echo esc_attr($color_secondary) ?>;
+        --dark: <?php echo esc_attr($color_dark) ?>;
     }
 </style>
 <div class="scroll_set_height"></div>
@@ -24,6 +27,5 @@ foreach ($section_group as $section) {
     $section_id = $section['select_section'];
     echo do_shortcode('[themeatelier-core id=' . $section_id . ']');
 }
-
 
 get_template_part('cwp-footer');
