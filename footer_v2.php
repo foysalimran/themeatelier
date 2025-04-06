13b35b57336569d8e6ebc5244628bd0d315c97ac
foysalimran <fimran2011@gmail.com>
2025-04-06 11:27:29 +0600

    update

--- footer_v2.php
+++ footer_v2.php
@@ -1,4 +1,15 @@
+<!--====== Footer section start ======-->
+<footer class="ta-footer<?php if (!(is_front_page()  ||is_page('Deals'))) : ?> bg-secondary<?php endif; ?>">
 
+<div class="container">
+  
+  <div class="pt-5 pb-6 text-center border-t border-secondary ta-footer-bottom">
+    <p>&copy; <span class="text-primary">Themeatelier </span>
+      <span id="currentYear"></span>. All rights reserved.
+    </p>
+  </div>
+</div>
+</footer>
 <!--====== Footer section end ======-->
 
 <?php wp_footer(); ?>
