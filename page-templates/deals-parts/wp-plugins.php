a318c36684ebe9cc9c2047907348ea1f82cc2446
foysalimran <fimran2011@gmail.com>
2025-06-25 12:47:13 +0600

    update

--- page-templates/deals-parts/wp-plugins.php
+++ page-templates/deals-parts/wp-plugins.php
@@ -14,7 +14,7 @@
                 <div class="p-5 px-[30px] pb-[26px]">
                     <div class="flex items-start justify-between gap-3 mb-5">
                         <div class="inline-block p-1 bg-white border rounded border-slate-200">
-                            <a href="https://darkifywp.com/pricing" target="_blank">
+                            <a href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=820&edd_options%5Bprice_id%5D=1&discount=SUMMER30" target="_blank">
                                 <img src="https://ps.w.org/darkify/assets/icon-256x256.gif?rev=3301202" class="max-w-[100px] rounded-sm" />
                             </a>
                         </div>
@@ -29,7 +29,7 @@
                     </div>
 
                     <h3 class="mt-3 mb-2 text-2xl leading-6 leading-8">
-                        <a target="_blank" class="text-font-color hover:text-[#171717] transition-all duration-300" href="https://darkifywp.com/pricing">Darkify -WordPress Dark Mode Plugin</a>
+                        <a target="_blank" class="text-font-color hover:text-[#171717] transition-all duration-300" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=820&edd_options%5Bprice_id%5D=1&discount=SUMMER30">Darkify -WordPress Dark Mode Plugin</a>
                     </h3>
                     <p class="text-lg ">
                         Darkify – is an extremely advanced dark mode plugin for any WordPress website. The plugin has the option to enable a dark mode switcher for the front end and also WordPress admin.
@@ -37,7 +37,7 @@
 
                     <div class="flex gap-2">
                       
-                        <a target="_blank" href="https://darkifywp.com/pricing" class="ta-btn-primary !bg-[#171717] hover:!bg-[#000000]">Get Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=820&edd_options%5Bprice_id%5D=1&discount=SUMMER30" class="ta-btn-primary !bg-[#171717] hover:!bg-[#000000]">Get Now</a>
                     </div>
                 </div>
             </div>
@@ -50,7 +50,7 @@
                 <div class="p-5 px-[30px] pb-[26px]">
                     <div class="flex items-start justify-between gap-3 mb-5">
                         <div class="inline-block p-1 bg-white border rounded border-slate-200">
-                            <a href="https://themeatelier.net/downloads/whatsapp-chat-help/#pricing" target="_blank">
+                            <a href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options%5Bprice_id%5D=1&discount=SUMMER30" target="_blank">
                                 <img src="https://ps.w.org/chat-help/assets/icon-256x256.png?rev=3270129" class="max-w-[100px] rounded-sm" />
                             </a>
                         </div>
@@ -71,14 +71,14 @@
                     </div>
 
                     <h3 class="mt-3 mb-2 text-2xl leading-6 leading-8">
-                        <a target="_blank" class="text-font-color hover:text-[#148C7F] transition-all duration-300" href="https://themeatelier.net/downloads/whatsapp-chat-help/#pricing">WhatsApp Chat Help - Chat Support Plugin</a>
+                        <a target="_blank" class="text-font-color hover:text-[#148C7F] transition-all duration-300" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options%5Bprice_id%5D=1&discount=SUMMER30">WhatsApp Chat Help - Chat Support Plugin</a>
                     </h3>
                     <p class="text-lg ">
                     Whatsapp chat support is a WordPress plugin that allows you to easily add WhatsApp chat bubble, buttons & WooCommerce whatsapp buttons to your website.
                     </p>
 
                     <div class="flex gap-2">
-                        <a target="_blank" href="https://themeatelier.net/downloads/whatsapp-chat-help/#pricing" class="ta-btn-primary !bg-[#148C7F] hover:!bg-[#000000]">Get Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=534&edd_options%5Bprice_id%5D=1&discount=SUMMER30" class="ta-btn-primary !bg-[#148C7F] hover:!bg-[#000000]">Get Now</a>
                     </div>
                 </div>
             </div>
@@ -90,7 +90,7 @@
                 <div class="p-5 px-[30px] pb-[26px]">
                     <div class="flex items-start justify-between gap-3 mb-5">
                         <div class="inline-block p-1 bg-white border rounded border-slate-200">
-                            <a href="https://themeatelier.net/downloads/greet-bubble/#pricing" target="_blank">
+                            <a href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=766&edd_options%5Bprice_id%5D=1&discount=SUMMER30" target="_blank">
                                 <img src="https://ps.w.org/greet-bubble/assets/icon-256x256.png?rev=3153272" class="max-w-[100px] rounded-sm" />
                             </a>
                         </div>
@@ -105,7 +105,7 @@
                     </div>
 
                     <h3 class="mt-3 mb-2 text-2xl leading-6 leading-8">
-                        <a target="_blank" class="text-font-color hover:text-[#7535FB] transition-all duration-300" href="https://themeatelier.net/downloads/greet-bubble/#pricing">Greet Bubble - Video Bubble Plugin for WordPress</a>
+                        <a target="_blank" class="text-font-color hover:text-[#7535FB] transition-all duration-300" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=766&edd_options%5Bprice_id%5D=1&discount=SUMMER30">Greet Bubble - Video Bubble Plugin for WordPress</a>
                     </h3>
 
                     <p class="text-lg ">
@@ -113,7 +113,7 @@
                     </p>
 
                     <div class="flex gap-2">
-                        <a target="_blank" href="https://themeatelier.net/downloads/greet-bubble/#pricing" class="ta-btn-primary !bg-[#7535fb] hover:!bg-[#000000]">Get Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=766&edd_options%5Bprice_id%5D=1&discount=SUMMER30" class="ta-btn-primary !bg-[#7535fb] hover:!bg-[#000000]">Get Now</a>
                     </div>
                 </div>
             </div>
@@ -125,7 +125,7 @@
                 <div class="p-5 px-[30px] pb-[26px]">
                     <div class="flex items-start justify-between gap-3 mb-5">
                         <div class="inline-block p-1 bg-white border rounded border-slate-200">
-                            <a href="https://themeatelier.net/downloads/eventful#pricing" target="_blank">
+                            <a href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=728&edd_options%5Bprice_id%5D=1&discount=SUMMER30" target="_blank">
                                 <img src="https://ps.w.org/eventful/assets/icon-256x256.png?rev=3088585" class="max-w-[100px] rounded-sm" />
                             </a>
                         </div>
@@ -140,14 +140,14 @@
                     </div>
 
                     <h3 class="mt-3 mb-2 text-2xl leading-6 leading-8">
-                        <a target="_blank" class="text-font-color hover:text-[#263AD0] transition-all duration-300" href="https://themeatelier.net/downloads/eventful#pricing">Eventful – Events Showcase For The Events Calendar</a>
+                        <a target="_blank" class="text-font-color hover:text-[#263AD0] transition-all duration-300" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=728&edd_options%5Bprice_id%5D=1&discount=SUMMER30">Eventful – Events Showcase For The Events Calendar</a>
                     </h3>
                     <p class="text-lg ">
                    With "Eventful," you can effortlessly create intelligent layouts for "The Events Calendar" plugin, effectively addressing and resolving compatibility issues that may arise.
                     </p>
 
                     <div class="flex gap-2">
-                        <a target="_blank" href="https://themeatelier.net/downloads/eventful#pricing" class="ta-btn-primary !bg-[#263AD0] hover:!bg-[#000000]">Get Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=728&edd_options%5Bprice_id%5D=1&discount=SUMMER30" class="ta-btn-primary !bg-[#263AD0] hover:!bg-[#000000]">Get Now</a>
                     </div>
                 </div>
             </div>
@@ -158,7 +158,7 @@
                 <div class="p-5 px-[30px] pb-[26px]">
                     <div class="flex items-start justify-between gap-3 mb-5">
                         <div class="inline-block p-1 bg-white border rounded border-slate-200">
-                            <a href="https://themeatelier.net/downloads/domain-for-sale/#pricing" target="_blank">
+                            <a href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=700&edd_options%5Bprice_id%5D=1&discount=SUMMER30" target="_blank">
                                 <img src="https://ps.w.org/domain-for-sale/assets/icon-256x256.png?rev=3171219" class="max-w-[100px] rounded-sm" />
                             </a>
                         </div>
@@ -174,14 +174,14 @@
                     </div>
 
                     <h3 class="mt-3 mb-2 text-2xl leading-6 leading-8">
-                        <a target="_blank" class="text-font-color hover:text-[#19BC9E] transition-all duration-300" href="https://themeatelier.net/downloads/domain-for-sale/#pricing">Domain For Sale Plugin for WordPress</a>
+                        <a target="_blank" class="text-font-color hover:text-[#19BC9E] transition-all duration-300" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=700&edd_options%5Bprice_id%5D=1&discount=SUMMER30">Domain For Sale Plugin for WordPress</a>
                     </h3>
                     <p class="text-lg ">
                     The ultimate WordPress plugin for domain sales, appraisals, auctions, and marketplace management.
                     </p>
 
                     <div class="flex gap-2">
-                        <a target="_blank" href="https://themeatelier.net/downloads/domain-for-sale/#pricing" class="ta-btn-primary !bg-[#19BC9E] hover:!bg-[#000000]">Get Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=700&edd_options%5Bprice_id%5D=1&discount=SUMMER30" class="ta-btn-primary !bg-[#19BC9E] hover:!bg-[#000000]">Get Now</a>
                     </div>
                 </div>
             </div>
@@ -194,7 +194,7 @@
                 <div class="p-5 px-[30px] pb-[26px]">
                     <div class="flex items-start justify-between gap-3 mb-5">
                         <div class="inline-block p-1 bg-white border rounded border-slate-200">
-                            <a href="https://themeatelier.net/downloads/eventful-for-elementor/#pricing" target="_blank">
+                            <a href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=768&edd_options%5Bprice_id%5D=1&discount=SUMMER30" target="_blank">
                                 <img src="https://ps.w.org/eventful-for-elementor/assets/icon-256x256.png?rev=3236337" class="max-w-[100px] rounded-sm" />
                             </a>
                         </div>
@@ -209,14 +209,14 @@
                     </div>
 
                     <h3 class="mt-3 mb-2 text-2xl leading-6 leading-8">
-                        <a target="_blank" class="text-font-color hover:text-[#263AD0] transition-all duration-300" href="https://themeatelier.net/downloads/eventful-for-elementor/#pricing">Eventful for Elementor – Events Showcase For The Events Calendar</a>
+                        <a target="_blank" class="text-font-color hover:text-[#263AD0] transition-all duration-300" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=768&edd_options%5Bprice_id%5D=1&discount=SUMMER30">Eventful for Elementor – Events Showcase For The Events Calendar</a>
                     </h3>
                     <p class="text-lg ">
                    With "Eventful for Elementor" you can seamlessly showcase events from The Events Calendar in Elementor with customizable widgets and dynamic layouts.
                     </p>
 
                     <div class="flex gap-2">
-                        <a target="_blank" href="https://themeatelier.net/downloads/eventful-for-elementor/#pricing" class="ta-btn-primary !bg-[#263AD0] hover:!bg-[#000000]">Get Now</a>
+                        <a target="_blank" href="https://themeatelier.net/checkout?edd_action=add_to_cart&download_id=768&edd_options%5Bprice_id%5D=1&discount=SUMMER30" class="ta-btn-primary !bg-[#263AD0] hover:!bg-[#000000]">Get Now</a>
                     </div>
                 </div>
             </div>
